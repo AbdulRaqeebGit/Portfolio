5f4473bd1c19fb2c1b44d7be53456215d65a19bb
AbdulRaqeebGit <abdulraqeebkumhar@gmail.com>
2025-09-05 15:45:12 +0500

    initial commit

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Carosal from '../components/Carusor'
 const Projects = () => {
   return (
     <div>
       <h1>Projects</h1>
+      <Carosal/>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // bas repo ka naam slash ke andar
+  base: '/Portfolio', // bas repo ka naam slash ke andar
 })
